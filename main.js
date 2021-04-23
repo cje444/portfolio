c565d4d35e88a732e70a085a837a40b7a9a873f7
cje444 <cje444@naver.com>
2021-04-22 21:02:56 -0400

    JC project-filtering

--- main.js
+++ main.js
@@ -57,3 +57,28 @@ document.addEventListener("scroll", () => {
 arrowUp.addEventListener("click", () => {
   scrollIntoView("#home");
 });
+
+//Projects
+const workBtnContainer = document.querySelector(".work__coategories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  projectContainer.classList.add("anime-out");
+
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter == "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+
+    projectContainer.classList.remove("anime-out");
+  }, 300);
+});
